4ebb76a582d6f9222656d1a97ef191bece9cdcb5
lance-malaga <lmalaga@my.bcit.ca>
2024-02-25 23:03:56 -0800

    add header and custom head components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from './Header.module.scss'
+
+import Logo from '../../../public/logo-black.svg'
+
+export default function Header(){
+
+    return (
+        <header className={styles.header__container}> 
+            <div className={styles.logo}>
+                <Link href={'/'}>
+                    <Image src={Logo} alt="logo-black" width={20} height={21}/>
+                    <p>VENTURE</p>
+                </Link>
+            </div>
+        </header>
+    )
+}
