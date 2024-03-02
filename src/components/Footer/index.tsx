748de4c4c40051e74719c8f92687638a1e209a8c
lance-malaga <lmalaga@my.bcit.ca>
2024-03-02 13:59:21 -0800

    add footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer__container}>
+            Venture | Landing Page 2024
+        </footer>
+    )
+}
